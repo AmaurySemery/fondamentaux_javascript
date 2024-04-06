f177544849a462994b86be9b041f1e4ae3ac23fc
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 01:36:52 +0200

    Mise en forme

--- src/algorithm/findLongestWordLength.js
+++ src/algorithm/findLongestWordLength.js
@@ -8,6 +8,6 @@ function findLongestWordLength(str) {
     str = str.split(' ');
     const wordLengthArr = str.map(word => word = word.length);
     return Math.max(...wordLengthArr);
-  }
-  
-  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
