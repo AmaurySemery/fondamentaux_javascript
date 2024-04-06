ede99cce7e568681497843a6d40f67df76f9cb60
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-06 18:53:47 +0200

    find longest word length

---
+++ src/algorithm/findLongestWordLength.js
@@ -0,0 +1,13 @@
+/*
+Renvoie la longueur du mot le plus long de la phrase fournie.
+
+Votre réponse doit être un nombre.
+*/
+
+function findLongestWordLength(str) {
+    str = str.split(' ');
+    const wordLengthArr = str.map(word => word = word.length);
+    return Math.max(...wordLengthArr);
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
